93439cba50b2140e3eb87605ba232a69ee8fd6a3
slash6475 <slash6475@slash-computer.staff.ece.fr>
2013-10-10 14:31:05 +0200

    Fix some previous echo

--- install.sh
+++ install.sh
@@ -16,29 +16,29 @@ else
 	DEST="$1"
 fi
 
-echo "Installing 'emma-cooja' ..."
-
 # Patch Contiki OS
 #patch -p2 --directory=$DEST --input=$THIS/patch/cooja_avrora-serial-socket_patch.diff
  
 # Copy COOJA File
-echo -ne "Copying COOJA files"
+echo -e "Copying COOJA files"
 echo -e  "==================="
 cp -R $THIS/src/* $DEST/
 cp $THIS/start.sh $DEST
+echo -e ""
 
 # Compiling tunslip
-echo -ne "Tunslip6 compilation"
-echo -e  "===================="
+echo -e "Tunslip6 compilation"
+echo -e  "==================="
 cd $DEST/tools/
 gcc tunslip6.c -o tunslip6
 cd $THIS
+echo -e ""
 
 # Copy Simulation file
-echo -ne "Copying simulation files"
-echo -e  "========================"
+echo -e "Copying simulation files"
+echo -e  "======================="
 mkdir $DEST/../simulations
 cp -r $THIS/simulations/* $DEST/../simulations/
-
+echo -e ""
 
 echo "Done."
